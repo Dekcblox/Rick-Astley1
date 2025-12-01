5cf40f27f36facc0f081d25b34cb423c0c2b2e10
Dekcblox <128036862+Dekcblox@users.noreply.github.com>
2025-12-01 13:29:44 +0900

    Update rickandastly.py

--- rickandastly.py
+++ rickandastly.py
@@ -1,7 +1,8 @@
 import webbrowser
 
 # 열고 싶은 이미지 링크
-IMAGE_URL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.ruliweb.com%2Fcommunity%2Fboard%2F300145%2Fread%2F21290868&psig=AOvVaw22QtVMy7q6O3afNrY01ESm&ust=1764649710265000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMi1zMvGm5EDFQAAAAAdAAAAABAE"
+IMAGE_URL = "https://i2.ruliweb.com/img/5/3/7/9/5379F4893A290A0017"
 
 # 기본 웹브라우저로 이미지 열기
 webbrowser.open(IMAGE_URL)
+
