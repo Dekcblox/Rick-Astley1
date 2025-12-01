78b01fdc9779ae993ae56c80dd2d36a565a20fab
Dekcblox <128036862+Dekcblox@users.noreply.github.com>
2025-12-01 13:28:43 +0900

    Update rickandastly.py

--- rickandastly.py
+++ rickandastly.py
@@ -1,4 +1,7 @@
 import webbrowser
 
-url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-webbrowser.open(url)
+# 열고 싶은 이미지 링크
+IMAGE_URL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.ruliweb.com%2Fcommunity%2Fboard%2F300145%2Fread%2F21290868&psig=AOvVaw22QtVMy7q6O3afNrY01ESm&ust=1764649710265000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMi1zMvGm5EDFQAAAAAdAAAAABAE"
+
+# 기본 웹브라우저로 이미지 열기
+webbrowser.open(IMAGE_URL)
