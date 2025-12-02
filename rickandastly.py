cba0b811054309dafc12afd8ca141acf32cc9fcd
Dekcblox <128036862+Dekcblox@users.noreply.github.com>
2025-12-02 09:21:31 +0900

    Update rickandastly.py

--- rickandastly.py
+++ rickandastly.py
@@ -1,8 +1,9 @@
 import webbrowser
 
 # 열고 싶은 이미지 링크
-IMAGE_URL = "https://i2.ruliweb.com/img/5/3/7/9/5379F4893A290A0017"
+IMAGE_URL = "https://i.pinimg.com/564x/2f/c5/42/2fc5429cc937124e13e1b4ff059bc1c5.jpg"
 
 # 기본 웹브라우저로 이미지 열기
 webbrowser.open(IMAGE_URL)
+
 
